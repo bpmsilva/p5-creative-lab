3bce27cf59d8154766852adf55bac7b3efc90987
Bernardo <bpmsilva@gmail.com>
2024-04-11 00:31:46 -0300

    add slider and text to thin red line

--- thin-red-line/sketch.js
+++ thin-red-line/sketch.js
@@ -1,15 +1,33 @@
 var canvasSize = 500;
 var step = canvasSize/10;
 var largeNumber = canvasSize+100;
-var sx = 1.5;
-var sy = 2;
+var oldAlpha = 0.5;
+var oldOmega = 14.1347251417346937;
 
 
 function setup() {
+  randomSeed(0);
   createCanvas(canvasSize, canvasSize);
 
-  strokeWeight(3);
+  sliderAlpha = createSlider(0, 3, oldAlpha, 0.001);
+  sliderAlpha.position(step/2, 10);
+  sliderAlpha.size(canvasSize/2 - 2*step);
+
+  sliderOmega = createSlider(0, 20, oldOmega, 0.001);
+  sliderOmega.position(step/2, 50);
+  sliderOmega.size(canvasSize/2 - 2*step);
+
+  draw_background();
+  RiemannZeta(oldAlpha, oldOmega);
+}
+
+
+draw_background = function() {
   background(255);
+  strokeWeight(3);
+
+  c = color(0, 0, 0);
+  stroke(c);
 
   // x axis
   line(
@@ -26,13 +44,6 @@ function setup() {
   line(canvasSize/2+step, 0, canvasSize/2+step, canvasSize);
 
   drawThinRedLine();
-  RiemannZeta(sx, sy);
-  console.log("Done!");
-}
-
-
-function draw() {
-  // no animation
 }
 
 
@@ -42,6 +53,32 @@ function drawThinRedLine() {
 
   stroke(c);
   line(canvasSize/2, 0, canvasSize/2, canvasSize);
+}
+
+
+function writeText() {
+  fill(0);
+  stroke('white');
+  textSize(20);
+  let alpha = sliderAlpha.value();
+  let omega = sliderOmega.value();
+  text('alpha = ' + alpha, canvasSize - 150, 45);
+  text('omega = ' + omega, canvasSize - 150, 80);
+}
+
+
+function draw() {
+  let alpha = sliderAlpha.value();
+  let omega = sliderOmega.value();
+  
+  if (alpha != oldAlpha || omega != oldOmega) {
+    draw_background();
+    RiemannZeta(alpha, omega);
+  }
+  writeText();
+
+  oldAlpha = alpha;
+  oldOmega = omega;
 }
 
 
@@ -55,17 +92,17 @@ function RiemannZeta(alpha, omega, lim=10000) {
   var x0 = canvasSize/2 - step;
   var y0 = canvasSize/2;
 
-  let old_x = 0;
-  let curr_x = 0;
-  let old_y = 0;
-  let curr_y = 0;
+  let oldX = 0;
+  let currX = 0;
+  let oldY = 0;
+  let currY = 0;
 
   for (n = 1; n < lim+1; n++) {
-    let curr_r = Math.pow(1/n, alpha);
+    let currR = Math.pow(1/n, alpha);
 
     let angle = compute_angle(omega, n);
-    curr_x += curr_r*Math.cos(angle);
-    curr_y += curr_r*Math.sin(angle);
+    currX += currR*Math.cos(angle);
+    currY += currR*Math.sin(angle);
 
     // add color (each segment has its own color)
     c = color(random(255), random(255), random(255));
@@ -73,13 +110,13 @@ function RiemannZeta(alpha, omega, lim=10000) {
 
     // draw line
     line(
-      x0 + 2*step*old_x,
-      y0 - 2*step*old_y,
-      x0 + 2*step*curr_x,
-      y0 - 2*step*curr_y
+      x0 + 2*step*oldX,
+      y0 - 2*step*oldY,
+      x0 + 2*step*currX,
+      y0 - 2*step*currY
     );
 
-    old_x = curr_x;
-    old_y = curr_y;
+    oldX = currX;
+    oldY = currY;
   }
 }
