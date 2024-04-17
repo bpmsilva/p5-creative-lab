7a817714745e94e22391cc4c22aa39d217167998
Bernardo <bpmsilva@gmail.com>
2024-04-17 19:13:55 -0300

    use mass as input and to compute gravity

--- three-body-problem/sketch.js
+++ three-body-problem/sketch.js
@@ -1,18 +1,21 @@
 // This is a simple simulation of the n-body problem.
 
 // Inputs
-const canvasSize = 500;
-const circlesRadius = canvasSize/20;
 const circles = [];
-
-const gravitationalConstant = 100;
-const numParticles = 3;
+const canvasSize = 800;
+const massRadiusConstant = 10; 
+const gravitationalConstant = 10;
 const epsilon = 0.001;
+
+// masses (also the number of bodies)
+const masses = [1, 2, 4, 16];
 
 class Circle {
   // The bodies are represented as circles
-  constructor(r, x, y, velX, velY) {
-    this.r = r;
+  constructor(mass, x, y, velX, velY) {
+    
+    this.mass = mass;
+    this.r = massRadiusConstant * Math.pow(this.mass, 1/3);
     this.x = x;
     this.y = y;
     this.velX = velX;
@@ -43,11 +46,12 @@ function computeGravity() {
         let dy = circles[j].y - circles[i].y;
         let d = sqrt(dx*dx + dy*dy + epsilon);  // Avoid division by zero
         // mass is equal one for now
-        let force = 1*gravitationalConstant / (d * d);
+        let force = gravitationalConstant * circles[i].mass * circles[j].mass / (d * d);
         let fx = force * dx / d;
         let fy = force * dy / d;
-        circles[i].velX += fx;
-        circles[i].velY += fy;
+
+        circles[i].velX += fx / circles[i].mass;
+        circles[i].velY += fy / circles[i].mass;
       }
     }
   }
@@ -57,16 +61,15 @@ function setup() {
   createCanvas(canvasSize, canvasSize);
   background(255);
 
-
-
-  for (let i = 0; i < numParticles; i++){
+  for (let i = 0; i < masses.length; i++){
     let randomX = random(canvasSize);
     let randomY = random(canvasSize);
     let randomVelX = random(2) - 1;
     let randomVelY = random(2) - 1;
+    console.log(masses[i])
     circles.push(
       new Circle(
-        circlesRadius,
+        masses[i],
         randomX,
         randomY,
         randomVelX,
