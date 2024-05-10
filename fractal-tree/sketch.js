89b032d10176ff6d68c497b439595eb70c93bc7c
Bernardo <bpmsilva@gmail.com>
2024-05-09 21:21:35 -0300

    add angle sliders

--- fractal-tree/sketch.js
+++ fractal-tree/sketch.js
@@ -1,6 +1,8 @@
+// initial constants
 const canvasSize = 600;
 const firstBranchLength = 200;
 
+// initial values for the first branch
 const x0 = canvasSize / 2;
 const x1 = x0;
 const y0 = canvasSize;
@@ -8,15 +10,21 @@ const y1 = y0 - firstBranchLength;
 
 const maxEndBranches = 2048;
 const initialFractalRatio = 0.5;
-const fractalAngle = Math.PI / 4;
+const initialFractalAngle = Math.PI / 4;
 
+// variables for the size ratio sliders
 let ratioSlider1, ratioSlider2;
-
 let oldFractalRatio1 = 0;
+let oldFractalRatio2 = 0;
 let fractalRatio1 = initialFractalRatio;
 let fractalRatio2 = initialFractalRatio;
 
-
+// fractal angle slider
+let angleSlider1, angleSlider2;
+let oldFractalAngle1 = 0;
+let oldFractalAngle2 = 0;
+let fractalAngle1 = initialFractalAngle;
+let fractalAngle2 = initialFractalAngle;
 
 class Branch {
   constructor(start, end) {
@@ -48,7 +56,7 @@ class Branch {
 }
 
 // recursive function to draw branches
-function drawBranches(branchArr, fractalRatio1, fractalRatio2) {
+function drawBranches(branchArr, fractalRatio1, fractalRatio2, fractalAngle1, fractalAngle2) {
 
   if (branchArr.length > maxEndBranches) {
     return;
@@ -62,23 +70,23 @@ function drawBranches(branchArr, fractalRatio1, fractalRatio2) {
 
     let newBranch1 = new Branch(
       // Branch starts at the end of the previous branch
-      {x: branch.end.x, y: branch.end.y},
+      { x: branch.end.x, y: branch.end.y },
       {
         // New branch ends at a give ratio and angle from the previous branch
         // 2/3 is the ratio and Math.PI / 4 is the angle that need to be remove
         // as they are magic numbers. The ideia here is to make sliders to control them
-        x: branch.end.x + fractalRatio1 * size * Math.cos(angle + fractalAngle),
-        y: branch.end.y + fractalRatio1 * size * Math.sin(angle + fractalAngle)
+        x: branch.end.x + fractalRatio1 * size * Math.cos(angle + fractalAngle1),
+        y: branch.end.y + fractalRatio1 * size * Math.sin(angle + fractalAngle1)
       }
     );
 
     let newBranch2 = new Branch(
       // The second branch has the same properties as the first one,
       // but the angle is different
-      {x: branch.end.x, y: branch.end.y},
+      { x: branch.end.x, y: branch.end.y },
       {
-        x: branch.end.x + fractalRatio2 * size * Math.cos(angle - fractalAngle),
-        y: branch.end.y + fractalRatio2 * size * Math.sin(angle - fractalAngle)
+        x: branch.end.x + fractalRatio2 * size * Math.cos(angle - fractalAngle2),
+        y: branch.end.y + fractalRatio2 * size * Math.sin(angle - fractalAngle2)
       }
     );
 
@@ -90,7 +98,7 @@ function drawBranches(branchArr, fractalRatio1, fractalRatio2) {
     newBranches.push(newBranch2);
   }
 
-  drawBranches(newBranches, fractalRatio1, fractalRatio2);
+  drawBranches(newBranches, fractalRatio1, fractalRatio2, fractalAngle1, fractalAngle2);
 }
 
 function setup() {
@@ -101,26 +109,38 @@ function setup() {
   strokeWeight(2);
 
   ratioSlider1 = createSlider(0, 1, initialFractalRatio, 0.01);
-  ratioSlider2 = createSlider(0, 1, initialFractalRatio, 0.01);  
+  ratioSlider2 = createSlider(0, 1, initialFractalRatio, 0.01);
+
+  angleSlider1 = createSlider(0, Math.PI / 2, initialFractalAngle, 0.01);
+  angleSlider2 = createSlider(0, Math.PI / 2, initialFractalAngle, 0.01);
 }
 
 function draw() {
 
-  fractalRatio1 = ratioSlider1.value();   
+  fractalRatio1 = ratioSlider1.value();
   fractalRatio2 = ratioSlider2.value();
 
+  fractalAngle1 = angleSlider1.value();
+  fractalAngle2 = angleSlider2.value();
+
   // draw first branch
-  if (fractalRatio1 != oldFractalRatio1||
-      fractalRatio2 != oldFractalRatio2) {
+  if (fractalRatio1 != oldFractalRatio1 ||
+    fractalRatio2 != oldFractalRatio2 ||
+    fractalAngle1 != oldFractalAngle1 ||
+    fractalAngle2 != oldFractalAngle2
+  ) {
     background(255);
 
-    let firstBranch = new Branch({x: x0, y: y0}, {x: x1, y: y1});
+    let firstBranch = new Branch({ x: x0, y: y0 }, { x: x1, y: y1 });
     firstBranch.show();
 
     // draw next branches
-    drawBranches([firstBranch], fractalRatio1, fractalRatio2);
+    drawBranches([firstBranch], fractalRatio1, fractalRatio2, fractalAngle1, fractalAngle2);
 
     oldFractalRatio1 = fractalRatio1;
     oldFractalRatio2 = fractalRatio2;
+
+    oldFractalAngle1 = fractalAngle1;
+    oldFractalAngle2 = fractalAngle2;
   }
 }
